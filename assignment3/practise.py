9e383b444d1f5a3456b9d93b7ae66ae2786409e3
Kiattisak Petchmark <kiattisakpetchmark@MacBook-Pro-khxng-Kiattisak.local>
2019-07-18 08:40:12 +0700

    Add file for assignment3

---
+++ assignment3/practise.py
@@ -0,0 +1,13 @@
+print("\n------KIATTISAK------\n")
+
+my_list = [30,25,45]
+print("My List : ",my_list)
+print("Sum List :",sum(my_list))
+
+
+my_tuple = (32,45,7,16)
+print("\nMy Tuple :",my_tuple)
+
+set1 = {2,1,14,5,65,78,34,55,77,9,90,91,11,12}
+set2 = {5,43,21,87,77,9,4,23,43,18,12}
+print("\nIntersection : ",set1.intersection(set2))
