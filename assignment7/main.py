68b0cebfac5b79b25012ebc2eac94fd04b7e62ee
ktspm <famlove8725@hotmail.com>
2019-08-08 15:59:17 +0700

    assignment7

--- assignment7/main.py
+++ assignment7/main.py
@@ -1,9 +1,14 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route("/")
 def member():
     return render_template('add.html')
+
+@app.route("/", methods=["post"])
+def add():
+    return 'xyz';
 
 if __name__ == "__main__":
     app.run(debug=True)
