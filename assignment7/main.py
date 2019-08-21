3597f14001663ac02cf66a2ab9fd90bf5bca7480
ktspm <famlove8725@hotmail.com>
2019-08-21 19:31:20 +0700

    assignment7

--- assignment7/main.py
+++ assignment7/main.py
@@ -1,18 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask,render_template , request
+from wtforms import StringField,PasswordField
 from flask_wtf import FlaskForm
-from wtforms import SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired, Length, Email
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'any secret string'
 
+class RegisterForm(FlaskForm):
+    name = StringField("name", validators=[InputRequired()])
+    last = StringField("last", validators=[InputRequired()])
+    email = StringField("Email",  [InputRequired("Please enter your email address."), Email("Please enter your email again.")])
+    username = StringField("username", validators=[InputRequired()])
+    password = PasswordField("password", validators=[InputRequired(), Length(min=8,max=12, message="Please enter your password 8-12 characters.")])
 
-@app.route("/")
+@app.route('/')
 def member():
-    return render_template('add.html')
+    form = RegisterForm()
+    return render_template('add.html', form=form)
 
-@app.route("/", methods=["post"])
+
+@app.route('/register', methods=["GET", "POST"])
 def register():
-    return 'Register Ok';
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return "Register OK"
+    return render_template('add.html', form=form)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(debug=True)
